55c1a039bf008e81e73c60c17f783c38260d1c9b
Daanish Karim <daanish.karim@gmail.com>
2023-03-15 09:57:11 +0000

    Refactoring to multimodule maven project. Closes #1

--- pet-clinic-web/src/main/java/com/springframework/petclinicweb/SpringPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinicweb/SpringPetClinicApplication.java
@@ -1,4 +1,4 @@
-package com.springframework.springpetclinic;
+package com.springframework.petclinicweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
